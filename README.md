98eb5da0a2214187cc30df74ebce03c8bb3cbd7e
saja123 <saja_fawaz@yahoo.com>
2024-04-23 21:14:24 +0300

    first database

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Movies-Library
+# Project Name - Project Version
+
+**Author Name**: Saja Fawaz
+
+## WRRC
+Add an image of your WRRC here
+!["WRRC"](./images/wrrc.jpeg)
+
+## Overview
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Project Features
+<!-- What are the features included in you app -->
---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const express = require("express");
+const server = express();
+const cors = require("cors");
+ dataInfo = require("./Movie_Data/data.json");
+ const PORT = 3000;
+ server.use(cors());
+ 
+ server.get("/", handleHomepage);
+ server.get("/favorite", handleFavoritepage);
+ server.use(handle404page);
+ server.use(handle500page);
+
+ function handleHomepage(req, res){
+    let fawaz = new Movie(dataInfo.title, dataInfo.poster_path, dataInfo.overview);
+    server.status(200).json(data);
+ }
+
+function  handleFavoritePage(req, res){
+    server.status(200).send("Welcome to Favorite Page");
+}
+
+ function Movie(title, poster_path, overview){
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+ }
+
+ function handle404page(req, res){
+    server.status(404).send("Page not found error");
+ }
+
+ function handle500page(req, res){
+    server.status(505).send({
+        "status": 500,
+        "responseText": "Sorry, something went wrong"
+    })
+ }
+
+ server.listen(PORT, () => {
+    console.log(`The server is up to listen in ${PORT}`);
+ })
+
+
+
+
+    
+
+  
+    
+
+
+
+
+ 
